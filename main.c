b63fc4edb5093c0956451ff07b5c6e282f5ab33a
neronecoder <neronecoder@gmail.com>
2021-01-19 03:38:12 +0000

    Add parser, code generator

--- main.c
+++ main.c
@@ -5,6 +5,17 @@
 #include <stdbool.h>
 #include <string.h>
 
+// Common util methods
+void error(char *fmt, ...)
+{
+    va_list ap;
+    va_start(ap, fmt);
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+    exit(1);
+}
+
+// Tokenizer
 typedef enum
 {
     TK_PUNCT, // Punctuators
@@ -30,15 +41,6 @@ Token *new_token(TokenKind kind, char *start, char *end)
     tok->len = end - start;
     tok->loc = start;
     return tok;
-}
-
-void error(char *fmt, ...)
-{
-    va_list ap;
-    va_start(ap, fmt);
-    vfprintf(stderr, fmt, ap);
-    fprintf(stderr, "\n");
-    exit(1);
 }
 
 static bool equal(Token *tok, char *op)
@@ -91,7 +93,7 @@ static Token *tokenize(char *p)
         }
 
         // Punctuators
-        if (*p == '+' || *p == '-')
+        if (ispunct(*p))
         {
             cur->next = new_token(TK_PUNCT, p, p + 1);
             cur = cur->next;
@@ -105,6 +107,179 @@ static Token *tokenize(char *p)
     return head.next;
 }
 
+// Parser
+typedef enum
+{
+    ND_ADD, // +
+    ND_SUB, // -
+    ND_MUL, // *
+    ND_DIV, // /
+    ND_NUM, // integer
+} NodeKind;
+
+typedef struct Node Node;
+
+struct Node
+{
+    NodeKind kind;
+    Node *lhs;
+    Node *rhs;
+    int val;
+};
+
+Node *new_node(NodeKind kind)
+{
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = kind;
+    return node;
+}
+
+// binary operator +, -, *, /
+Node *new_binary(NodeKind kind, Node *lhs, Node *rhs)
+{
+    Node *node = new_node(kind);
+    node->lhs = lhs;
+    node->rhs = rhs;
+    return node;
+}
+
+// integer
+Node *new_num(int val)
+{
+    Node *node = new_node(ND_NUM);
+    node->val = val;
+    return node;
+}
+
+/* Grammar
+ * expr = mul ("+" mul | "-" mul)*
+ * mul  = primary ("*" primary | "/" primary)*
+ * primary = num | "(" expr ")"
+ */
+static Node *expr(Token **cur, Token *tok);
+static Node *mul(Token **cur, Token *tok);
+static Node *primary(Token **cur, Token *tok);
+
+// Steps
+// 1. Parse left hand side: mul0
+// 2. While there is a valid operator, make current node to be lhs and add it as rhs
+// 3. Keep applying the operation
+// 4. Once all parsing is done, move pointer that tracks the current token.
+//            +/-
+//             |
+//           -----
+//          |     |
+//         +/-    mul2
+//          |
+//        -----
+//       |     |
+//      mul0    mul1
+static Node *expr(Token **cur, Token *tok)
+{
+    Node *node = mul(&tok, tok);
+
+    for (;;)
+    {
+        if (equal(tok, "+"))
+        {
+            node = new_binary(ND_ADD, node, mul(&tok, tok->next));
+            continue;
+        }
+        if (equal(tok, "-"))
+        {
+            node = new_binary(ND_SUB, node, mul(&tok, tok->next));
+            continue;
+        }
+        *cur = tok;
+        return node;
+    }
+}
+
+static Node *mul(Token **cur, Token *tok)
+{
+    Node *node = primary(&tok, tok);
+
+    for (;;)
+    {
+        if (equal(tok, "*"))
+        {
+            node = new_binary(ND_MUL, node, mul(&tok, tok->next));
+            continue;
+        }
+        if (equal(tok, "/"))
+        {
+            node = new_binary(ND_DIV, node, mul(&tok, tok->next));
+            continue;
+        }
+        *cur = tok;
+        return node;
+    }
+}
+
+static Node *primary(Token **cur, Token *tok)
+{
+    if (tok->kind == TK_NUM)
+    {
+        Node *node = new_num(tok->val);
+        *cur = tok->next;
+        return node;
+    }
+
+    if (equal(tok, "("))
+    {
+        Node *node = expr(&tok, tok->next);
+        *cur = skip(tok, ")");
+        return node;
+    }
+
+    error("Expected an expression");
+}
+
+// Code Generator
+
+// Generated Code is based on Stack machine.
+// For example, add operator works as if take 2 inputs from stack top by popping 2 values,
+// then add them and push the result back on top of stack.
+// Add, Sub, Mul, Div can be done in the same way, pop 2 values from stack top, compute result and push it on top of stack.
+
+//
+// pop <reg>: pop top of stack into <reg>
+// push <reg>: push <reg> value on top of stack
+static void gen_code(Node *node)
+{
+    if (node->kind == ND_NUM)
+    {
+        printf("    push %d\n", node->val);
+        return;
+    }
+
+    // First compute lhs and rhs, top 2 values on stack shuold be rhs, lhs, ...
+    gen_code(node->lhs);
+    gen_code(node->rhs);
+
+    printf("    pop rdi\n");
+    printf("    pop rax\n");
+
+    switch (node->kind)
+    {
+    case ND_ADD:
+        printf("    add rax, rdi\n");
+        break;
+    case ND_SUB:
+        printf("    sub rax, rdi\n");
+        break;
+    case ND_MUL:
+        printf("    imul rax, rdi\n");
+        break;
+    case ND_DIV:
+        printf("    cqo\n");
+        printf("    idiv rdi\n");
+        break;
+    }
+
+    printf("    push rax\n");
+}
+
 int main(int argc, char **argv)
 {
     if (argc != 2)
@@ -114,31 +289,21 @@ int main(int argc, char **argv)
     }
 
     Token *tok = tokenize(argv[1]);
+    Node *node = expr(&tok, tok);
+
+    if (tok->kind != TK_EOF) {
+        error("Extra token exists");
+    }
 
     // Code generation
     printf(".intel_syntax noprefix\n");
     printf(".globl main\n");
     printf("main:\n");
 
-    // First token has to be number here.
-    printf("    mov rax, %d\n", get_number(tok));
-    tok = tok->next;
+    // Traverse AST to emit assembly.
+    gen_code(node);
 
-    // ... followed by either '+ <number>' or '- <number>'
-    while (tok->kind != TK_EOF)
-    {
-        if (equal(tok, "+"))
-        {
-            printf("    add rax, %d\n", get_number(tok->next));
-            tok = tok->next->next;
-            continue;
-        }
-
-        // If token is not +, then it has to be - here.
-        tok = skip(tok, "-");
-        printf("    sub rax, %d\n", get_number(tok));
-        tok = tok->next;
-    }
+    printf("    pop rax\n");
     printf("    ret\n");
     return 0;
 }
